1b6e55debdb7324cd495873a0d023d10a586c3a3
caismith-ch <csmith2@companieshouse.gov.uk>
2020-04-20 13:23:48 +0100

    Implement skeleton certificate ordered screen

---
+++ src/model/page.urls.ts
@@ -0,0 +1,4 @@
+export const ORDERS: string = "/orders";
+// ORDER_COMPLETE is currently a dummy url
+export const ORDER_COMPLETE: string = "/orders/order-id/order-complete";
+export const BASKET: string = "/basket**";
