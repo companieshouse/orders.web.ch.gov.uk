feba13b66a0a9fa8e83f06496458ce7837e5ae2a
nabeelamirch <namir@companieshouse.gov.uk>
2020-05-20 15:39:32 +0100

    removed test config

--- src/config/config.ts
+++ src/config/config.ts
@@ -9,9 +9,9 @@ const getEnvironmentValue = (key: string, defaultValue?: any): string => {
     return value || defaultValue as string;
 };
 
-export const PIWIK_URL = getEnvironmentValue("PIWIK_URL", "tes");
+export const PIWIK_URL = getEnvironmentValue("PIWIK_URL");
 
-export const PIWIK_SITE_ID = getEnvironmentValue("PIWIK_SITE_ID", "tes");
+export const PIWIK_SITE_ID = getEnvironmentValue("PIWIK_SITE_ID");
 
 export const COOKIE_SECRET = getEnvironmentValue("COOKIE_SECRET");
 
