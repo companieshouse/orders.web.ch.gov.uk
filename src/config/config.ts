6d8aba1eb5bb4ae993fd1af5dcb3cc4f7c6c2a80
ethatcher83 <ethatcher@companieshouse.gov.uk>
2020-07-23 15:54:58 +0100

    updated order confirmation controller to handle certified copies pass variables from the order controller to the template dependent on order type updated order complete template to handle variables being passed from the controller updated integration test for order controller

--- src/config/config.ts
+++ src/config/config.ts
@@ -24,3 +24,9 @@ export const API_URL = getEnvironmentValue("API_URL");
 export const CHS_URL = getEnvironmentValue("CHS_URL");
 
 export const APPLICATION_NAME = "orders.web.ch.gov.uk";
+
+export const SERVICE_NAME_CERTIFIED_COPIES = "Order a certified document";
+
+export const SERVICE_NAME_CERTIFICATES = "Order a certificate";
+
+export const SERVICE_NAME_GENERIC = "";
