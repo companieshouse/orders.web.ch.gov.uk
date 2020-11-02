56784c2ae258e839fcd3b0f8bb19f032ed7f973c
ethatcher83 <ethatcher@companieshouse.gov.uk>
2020-11-02 13:17:50 +0000

    update text on confirmaton page for mid

--- src/service/map.item.service.ts
+++ src/service/map.item.service.ts
@@ -271,11 +271,10 @@ export const mapItem = (item: Item, deliveryDetails: DeliveryDetails| undefined)
             orderDetailsTable: missingImageDeliveryOrderDetails,
             titleText: "Document Requested",
             pageTitle: "Document Requested",
-            happensNext: `<p class="govuk-body">We will check if the document is available. This takes us up to 2 hours between 8.30am - 4pm, Monday - Friday (excluding holidays).</p>
+            happensNext: `<p class="govuk-body">It can take us several hours to check the availability of a document. We will aim to add it to the <a href="/company/${item.companyNumber}/filing-history" class="govuk-link govuk-link--no-visited-state">company’s filing history</a>
+            that day if the request is received between 8:30am and 3pm, Monday to Friday(excluding bank holidays).</p>
 
-            <p class="govuk-body">If your request has been made outside of this time, the document will be added the next working day.</p>
-            
-            <p class="govuk-body">Check the <a href="/company/${item.companyNumber}/filing-history" class="govuk-link govuk-link--no-visited-state">company’s filing history</a> for updates on the document's availablility.</p>
+            <p class="govuk-body">If you make a request after 3pm, we will add the document the next working day.</p>
             
             <p class="govuk-body">If we cannot add the document to the filing history, we will contact you to issue a refund.</p>`
         };
