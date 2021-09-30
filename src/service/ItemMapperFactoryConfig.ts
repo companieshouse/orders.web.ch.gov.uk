2df6ca6de5e46986f8364b083deae34fc9d39c13
phudson-ch <phudson@companieshouse.gov.uk>
2021-09-30 09:54:36 +0100

    Decouple ItemMapperFactor from feature flags
    * Refactor ItemMapperFactoryConfig to configure factory using feature flags

--- src/service/ItemMapperFactoryConfig.ts
+++ src/service/ItemMapperFactoryConfig.ts
@@ -1,16 +1,29 @@
-import { ITEM_MAPPER_FACTORY_CONFIG } from "config/config";
+import {ItemMapperFactory} from "../service/ItemMapperFactory";
+import {OtherCertificateItemMapper} from "../service/OtherCertificateItemMapper";
+import {CompanyType} from "../model/CompanyType";
+import {ItemMapper} from "../service/ItemMapper";
+import {LLPCertificateItemMapper} from "../service/LLPCertificateItemMapper";
+import {LPCertificateItemMapper} from "../service/LPCertificateItemMapper";
+
+type Config =  { llpCertificateOrdersEnabled: boolean, lpCertificateOrdersEnabled: boolean };
 
 export class ItemMapperFactoryConfig {
-
-    public constructor(private _config: { llpCertificateOrdersEnabled: string, lpCertificateOrdersEnabled: string }) {
-        
+    public constructor(private _config: Config) {
     }
 
-    set config(config: { llpCertificateOrdersEnabled: string, lpCertificateOrdersEnabled: string }) {
-        this._config = config;
+    getInstance = () => {
+        const typeSpecificItemMappers: [CompanyType,  () => ItemMapper][] = [];
+        if (this._config.llpCertificateOrdersEnabled) {
+            typeSpecificItemMappers.push([CompanyType.LIMITED_LIABILITY_PARTNERSHIP, () => { return new LLPCertificateItemMapper() }]);
+        }
+        if (this._config.lpCertificateOrdersEnabled) {
+            typeSpecificItemMappers.push([CompanyType.LIMITED_PARTNERSHIP, () => { return new LPCertificateItemMapper() }]);
+        }
+
+        return new ItemMapperFactory(typeSpecificItemMappers, () => {return new OtherCertificateItemMapper()});
     }
 
-    get config() {
-        return this._config;
+    set config(value: Config) {
+        this._config = value;
     }
 }
