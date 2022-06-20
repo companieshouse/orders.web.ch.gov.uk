bf52a5392b40b9b843b2b98e48750ad3c3d7e7e2
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2022-06-20 14:58:06 +0100

    GCI-2127: Get order ID from request orderId parameter via regex match.

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -6,8 +6,6 @@ import { createLogger } from "ch-structured-logging";
 import { APPLICATION_NAME } from "../config/config";
 import { UserProfileKeys } from "@companieshouse/node-session-handler/lib/session/keys/UserProfileKeys";
 import { BASKET, ORDER_COMPLETE, ORDERS, replaceOrderId } from "../model/page.urls";
-import { Checkout } from "@companieshouse/api-sdk-node/dist/services/order/checkout";
-import { getCheckout } from "../client/api.client";
 
 const logger = createLogger(APPLICATION_NAME);
 
@@ -16,53 +14,37 @@ REDIRECTS_WHITELIST[ORDERS] = ORDERS;
 REDIRECTS_WHITELIST[ORDER_COMPLETE] = ORDER_COMPLETE;
 REDIRECTS_WHITELIST[BASKET] = BASKET;
 
-export default async (req: Request, res: Response, next: NextFunction) => {
+export default (req: Request, res: Response, next: NextFunction) => {
     // TODO GCI-2127 We probably will not need to log these?
     logger.info(`req.originalUrl = ${req.originalUrl}`);
     logger.info(`req.params = ${JSON.stringify(req.params)}`);
     logger.info(`req.query = ${JSON.stringify(req.query)}`);
-    try {
-        if (!req.session) {
-            logger.debug(`${req.url}: Session object is missing!`);
-        }
-        const signedIn = req.session?.data?.[SessionKey.SignInInfo]?.[SignInInfoKeys.SignedIn] === 1;
 
-        if (!signedIn) {
-            const signInInfo = req.session?.data[SessionKey.SignInInfo];
-            const accessToken = signInInfo?.[SignInInfoKeys.AccessToken]?.[SignInInfoKeys.AccessToken]!;
-            const orderId = req.params.orderId;
-            // Get the order ID from an API call response, rather than using the one obtained from the incoming request.
-            const checkout: Checkout = await getCheckout(accessToken, orderId);
-            if (checkout && checkout.reference) {
-                const orderIdFromApi = checkout.reference;
-                const redirection = `/signin?return_to=${getWhitelistedReturnToURL(req, orderIdFromApi)}`;
-                logger.info(`User unauthorized, status_code=401, Redirecting to: ${redirection}`);
-                return res.redirect(redirection);
-            } else {
-                const error = `Unable to get checkout from checkout API using orderId path parameter ${orderId}`;
-                logger.error(error);
-                throw new Error(error);
-            }
-        } else {
-            const signInInfo = req.session?.data[SessionKey.SignInInfo];
-            const userId = signInInfo?.[SignInInfoKeys.UserProfile]?.[UserProfileKeys.UserId];
-            logger.info(`User is signed in, user_id=${userId}`);
-        }
-        next();
-    } catch (err) {
-        logger.error(`Authentication middleware: ${err}`);
-        next(err);
+    if (!req.session) {
+        logger.debug(`${req.url}: Session object is missing!`);
     }
+    const signedIn = req.session?.data?.[SessionKey.SignInInfo]?.[SignInInfoKeys.SignedIn] === 1;
+
+    if (!signedIn) {
+        const redirection = `/signin?return_to=${getWhitelistedReturnToURL(req)}`;
+        logger.info(`User unauthorized, status_code=401, Redirecting to: ${redirection}`);
+        return res.redirect(redirection);
+    } else {
+        const signInInfo = req.session?.data[SessionKey.SignInInfo];
+        const userId = signInInfo?.[SignInInfoKeys.UserProfile]?.[UserProfileKeys.UserId];
+        logger.info(`User is signed in, user_id=${userId}`);
+    }
+    next();
 };
 
-export const getWhitelistedReturnToURL = (req: Request, orderIdFromApi: string) => {
+export const getWhitelistedReturnToURL = (req: Request) => {
     const returnToUrl = req.originalUrl;
     logger.info(`Looking up return to URL ${returnToUrl} in whitelist.`);
     if (returnToUrl in REDIRECTS_WHITELIST) {
         logger.info(`Found return to URL ${returnToUrl} in whitelist.`);
         return REDIRECTS_WHITELIST[returnToUrl];
     } else if (returnToUrl.startsWith(ORDERS) && returnToUrl.length > ORDERS.length) {
-        return rebuildOrderCompleteURL(req, orderIdFromApi);
+        return rebuildOrderCompleteURL(req);
     } else {
         const error = `Return to URL ${returnToUrl} not found in trusted URLs whitelist ${JSON.stringify(REDIRECTS_WHITELIST)}.`;
         logger.error(error);
@@ -70,10 +52,10 @@ export const getWhitelistedReturnToURL = (req: Request, orderIdFromApi: string) 
     }
 };
 
-const rebuildOrderCompleteURL = (req: Request, orderIdFromApi: string) => {
+const rebuildOrderCompleteURL = (req: Request) => {
     const queryString = Object.keys.length > 0
         ? "?" + Object.keys(req.query).map(key => key + "=" + req.query[key]).join("&") : "";
-    const orderCompleteURL = replaceOrderId(ORDER_COMPLETE, orderIdFromApi) + queryString;
+    const orderCompleteURL = replaceOrderId(ORDER_COMPLETE, getWellFormedOrderId3(req)) + queryString;
     logger.info(`Rebuilt order complete URL = ${orderCompleteURL}`);
     return orderCompleteURL;
 };
