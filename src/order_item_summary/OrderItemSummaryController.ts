573d64523cc009e5208ce9f00830b5d0611e8c36
dgroves-ch <dgroves@companieshouse.gov.uk>
2022-08-18 18:18:32 +0100

    Implement OrderSummaryController
    * OrderSummaryController handles an incoming request with session data
      attached and fetches order item from associated
      OrderItemSummaryService instance.

--- src/order_item_summary/OrderItemSummaryController.ts
+++ src/order_item_summary/OrderItemSummaryController.ts
@@ -1,5 +1,46 @@
 import { NextFunction, Request, Response } from "express";
+import { OrderItemSummaryService } from "./OrderItemSummaryService";
+import { SessionKey } from "@companieshouse/node-session-handler/lib/session/keys/SessionKey";
+import { SignInInfoKeys } from "@companieshouse/node-session-handler/lib/session/keys/SignInInfoKeys";
+import { UserProfileKeys } from "@companieshouse/node-session-handler/lib/session/keys/UserProfileKeys";
+import { NotFound, Unauthorized } from "http-errors";
+import { createLogger } from "ch-structured-logging";
+import { APPLICATION_NAME } from "../config/config";
+
+const logger = createLogger(APPLICATION_NAME);
 
 export class OrderItemSummaryController {
-    async viewSummary (request: Request, response: Response, next: NextFunction): Promise<void> {}
+
+    constructor (private service: OrderItemSummaryService = new OrderItemSummaryService()) {
+    }
+
+    async viewSummary (request: Request, response: Response, next: NextFunction): Promise<void> {
+        const orderId = request.params.orderId;
+        const itemId = request.params.itemId;
+        const signInInfo = request.session?.data[SessionKey.SignInInfo];
+        const apiToken = signInInfo?.[SignInInfoKeys.AccessToken]?.[SignInInfoKeys.AccessToken]!;
+        const userId = signInInfo?.[SignInInfoKeys.UserProfile]?.[UserProfileKeys.UserId];
+        try {
+            logger.debug(`Retrieving summary for order/item [${orderId}/${itemId}] for user [${userId}]...`);
+            const viewModel = await this.service.getOrderItem({
+                orderId,
+                itemId,
+                apiToken
+            });
+            logger.debug(`Retrieved summary for order/item [${orderId}/${itemId}] for user [${userId}]`);
+            return response.render(viewModel.template, viewModel.data);
+        } catch (error) {
+            if (error instanceof Unauthorized) {
+                logger.info(`User [${userId}] is not authorised to retrieve summary for order/item [${orderId}/${itemId}]`);
+                next();
+            } else if (error instanceof NotFound) {
+                logger.info(`Order/item [${orderId}/${itemId}] does not exist`);
+                next();
+            } else {
+                logger.error(`Error displaying summary for order/item [${orderId}/${itemId}] for user [${userId}]`);
+                next(error);
+            }
+        }
+
+    }
 }
