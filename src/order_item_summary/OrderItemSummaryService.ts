c81813bf6fd88660d575b38b7a9763c82d848285
dgroves-ch <dgroves@companieshouse.gov.uk>
2022-08-18 18:57:02 +0100

    Implement OrderItemSummaryService
    * OrderItemSummaryService responsible for fetching order item resource
      from orders.api.ch.gov.uk and mapping it to an OrderItemView via the
      mapper instance returned by OrderItemSummaryFactory.
    * Note: api-sdk-node needs to be rebuilt & api-sdk-node dependency needs
      to be updated prior to merging this in.

--- src/order_item_summary/OrderItemSummaryService.ts
+++ src/order_item_summary/OrderItemSummaryService.ts
@@ -1,8 +1,17 @@
 import { OrderItemRequest } from "./OrderItemRequest";
 import { OrderItemView } from "./OrderItemView";
+import { OrderItemSummaryFactory } from "./OrderItemSummaryFactory";
+import { getOrderItem } from "../client/api.client";
 
 export class OrderItemSummaryService {
+
+    constructor(private factory: OrderItemSummaryFactory = new OrderItemSummaryFactory()) {
+    }
+
     async getOrderItem (request: OrderItemRequest): Promise<OrderItemView> {
-        return undefined as any;
+        const orderItem = await getOrderItem(request.orderId, request.itemId, request.apiToken);
+        const mapper = this.factory.getMapper(orderItem);
+        mapper.map();
+        return mapper.getMappedOrder();
     }
 }
