f13c512fa47f6b29a0e7504d84cd60d9e3329ce4
ethatcher83 <ethatcher@companieshouse.gov.uk>
2022-08-25 14:53:52 +0100

    error page banner now has a title and link to homepage display error name instead of whole error object

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -1,18 +1,24 @@
 import { Request, Response, NextFunction } from "express";
 import { createLogger } from "ch-structured-logging";
+import { HttpError } from "http-errors";
 
-import { APPLICATION_NAME } from "../config/config";
+import { APPLICATION_NAME, CHS_URL } from "../config/config";
 import * as templatePaths from "../model/template.paths";
 
 const logger = createLogger(APPLICATION_NAME);
+const serviceName = "Find and update company information";
+const serviceUrl = CHS_URL;
 
 const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
     logger.error("Page not found: " + `${req.path}`);
-    return res.status(404).render(templatePaths.ERROR_NOT_FOUND);
+    return res.status(404).render(templatePaths.ERROR_NOT_FOUND, { serviceName, serviceUrl });
 };
 
 const errorHandler = (err: unknown, req: Request, res: Response, next: NextFunction) => {
-    res.status(500).render(templatePaths.ERROR, { errorMessage: err });
+    const errorStatusCode = err instanceof HttpError ? err?.statusCode : 500;
+    const errorName = err instanceof HttpError ? err?.name : "Error";
+    logger.error("Error: " + `${req.path}`);
+    res.status(errorStatusCode).render(templatePaths.ERROR, { errorMessage: errorName, serviceName, serviceUrl });
 };
 
 export default [notFoundHandler, errorHandler];
