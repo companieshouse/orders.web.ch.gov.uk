b195cd41a3008573dbdae48ea93d3ccdc3cb2137
aokunade <aokunade@companieshouse.gov.uk>
2022-12-02 08:23:22 +0000

    Added a path for add another document button

--- src/model/page.urls.ts
+++ src/model/page.urls.ts
@@ -6,7 +6,7 @@ export const BASKET: string = "/basket";
 export const BASKET_REMOVE: string = "/basket/remove/:itemId";
 export const DELIVERY_DETAILS: string = "/delivery-details";
 export const ORDER_ITEM_SUMMARY: string = "/orders/:orderId/items/:itemId";
-
+export const ADD_ANOTHER_DOCUMENT_PATH: string = "/add-another-document";
 export const replaceOrderId = (uri: string, orderId: string) => {
     return uri.replace(":orderId", orderId);
 };
