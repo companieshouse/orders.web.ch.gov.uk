faecaf473f803cf34c93b1ef1cd2f9ba34303a27
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2022-12-19 15:51:45 +0000

    GCI-2426: Formatting.

--- src/utils/page.header.utils.ts
+++ src/utils/page.header.utils.ts
@@ -4,9 +4,8 @@ import { SessionKey } from "@companieshouse/node-session-handler/lib/session/key
 import { SignInInfoKeys } from "@companieshouse/node-session-handler/lib/session/keys/SignInInfoKeys";
 
 export const mapPageHeader = (req: Request): PageHeader => {
-
     const userEmailAddress = req.session?.data?.signin_info?.user_profile?.email;
     const isSignedIn = req.session?.data?.[SessionKey.SignInInfo]?.[SignInInfoKeys.SignedIn] === 1;
 
-    return {userEmailAddress: userEmailAddress ,isSignedIn: isSignedIn};
-}
+    return { userEmailAddress: userEmailAddress, isSignedIn: isSignedIn };
+};
