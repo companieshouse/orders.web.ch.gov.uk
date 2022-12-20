52a0a4a55e1446a0eb299b92c241afdb1e38fdcf
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2022-12-20 09:26:10 +0000

    GCI-2426: Rationalise new unit tests.

--- src/test/utils/page.header.utils.test.ts
+++ src/test/utils/page.header.utils.test.ts
@@ -1,5 +1,3 @@
-// import sinon from "sinon";
-// import { signedInSessionData, SIGNED_IN_COOKIE } from "../__mocks__/redis.mocks";
 import chai from "chai";
 import { Request } from "express";
 import { Session } from "@companieshouse/node-session-handler";
@@ -8,35 +6,32 @@ import { PageHeader } from "../../model/PageHeader";
 
 const deliveryDetailsPath = "/delivery-details";
 const basketPath = "/basket";
-// TODO GCI-2426 Check this
-const confirmationPath = "/confirmation";
+const confirmationPath =
+    "/orders/ORD-811516-715265/confirmation?ref=orderable_item_ORD-811516-715265&state=9e307591-45c8-4dcd-b06d-2bf5e7d6c9d0&status=paid";
 const testEmailAddress: string = "test@testemail.com";
 
+// @SuppressWarnings("typescript:S2699")
 describe("mapPageHeader", () => {
     it("should populate the header correctly for the delivery details page", async () => {
-        const mockRequest = generateMockRequest(deliveryDetailsPath, 1);
-
-        const returnedPageHeader: PageHeader = mapPageHeader(mockRequest);
-        chai.expect(returnedPageHeader.isSignedIn).to.equal(true);
-        chai.expect(returnedPageHeader.userEmailAddress).to.equal(testEmailAddress);
+        testPageHeader(deliveryDetailsPath);
     });
 
     it("should populate the header correctly for the basket page", async () => {
-        const mockRequest = generateMockRequest(basketPath, 1);
-
-        const returnedPageHeader: PageHeader = mapPageHeader(mockRequest);
-        chai.expect(returnedPageHeader.isSignedIn).to.equal(true);
-        chai.expect(returnedPageHeader.userEmailAddress).to.equal(testEmailAddress);
+        testPageHeader(basketPath);
     });
 
     it("should populate the header correctly for the confirmation page", async () => {
-        const mockRequest = generateMockRequest(confirmationPath, 1);
-
-        const returnedPageHeader: PageHeader = mapPageHeader(mockRequest);
-        chai.expect(returnedPageHeader.isSignedIn).to.equal(true);
-        chai.expect(returnedPageHeader.userEmailAddress).to.equal(testEmailAddress);
+        testPageHeader(confirmationPath);
     });
 });
+
+const testPageHeader = (path: string) => {
+    const mockRequest = generateMockRequest(path, 1);
+
+    const returnedPageHeader: PageHeader = mapPageHeader(mockRequest);
+    chai.expect(returnedPageHeader.isSignedIn).to.equal(true);
+    chai.expect(returnedPageHeader.userEmailAddress).to.equal(testEmailAddress);
+};
 
 const generateMockRequest = (path: string, isSignedIn: number):Request => {
     const mockRequest = {
