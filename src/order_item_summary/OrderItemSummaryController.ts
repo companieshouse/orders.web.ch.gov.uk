8901417687f4523a6ec86f274baa024bb3635692
aokunade <aokunade@companieshouse.gov.uk>
2023-01-04 16:25:13 +0000

    removed unused variables

--- src/order_item_summary/OrderItemSummaryController.ts
+++ src/order_item_summary/OrderItemSummaryController.ts
@@ -21,16 +21,19 @@ export class OrderItemSummaryController {
         const signInInfo = request.session?.data[SessionKey.SignInInfo];
         const apiToken = signInInfo?.[SignInInfoKeys.AccessToken]?.[SignInInfoKeys.AccessToken]!;
         const userId = signInInfo?.[SignInInfoKeys.UserProfile]?.[UserProfileKeys.UserId];
+        const pageHeader = mapPageHeader(request)
+        logger.debug(`req = ${request}`)
         try {
             logger.debug(`Retrieving summary for order/item [${orderId}/${itemId}] for user [${userId}]...`);
-            const pageHeader = mapPageHeader(request);
             const viewModel = await this.service.getOrderItem({
                 orderId,
                 itemId,
                 apiToken
             });
+            logger.debug(`isSignedIn = ${pageHeader.isSignedIn}`)
+            logger.debug(`userEmailAddress = ${pageHeader.userEmailAddress}`)
             logger.debug(`Retrieved summary for order/item [${orderId}/${itemId}] for user [${userId}]`);
-            return response.render(viewModel.template,  { ...viewModel.data, ...pageHeader })
+            return response.render(viewModel.template,  {...viewModel.data, ...pageHeader })
         } catch (error) {
             if (error instanceof Unauthorized) {
                 logger.info(`User [${userId}] is not authorised to retrieve summary for order/item [${orderId}/${itemId}]`);
