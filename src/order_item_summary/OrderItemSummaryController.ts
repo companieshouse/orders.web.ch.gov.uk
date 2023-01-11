72f113a98b85c7bb9f91fb979003486efd6d9aca
aokunade <aokunade@companieshouse.gov.uk>
2023-01-11 09:16:19 +0000

    Add basketLink to order item details page

--- src/order_item_summary/OrderItemSummaryController.ts
+++ src/order_item_summary/OrderItemSummaryController.ts
@@ -7,6 +7,9 @@ import { NotFound, Unauthorized } from "http-errors";
 import { createLogger } from "ch-structured-logging";
 import { APPLICATION_NAME } from "../config/config";
 import { mapPageHeader } from "../utils/page.header.utils";
+import { Basket } from "@companieshouse/api-sdk-node/dist/services/order/basket";
+import { getBasket } from "../client/api.client";
+import { BasketLink, getBasketLink } from "../utils/basket.util";
 
 const logger = createLogger(APPLICATION_NAME);
 
@@ -24,13 +27,15 @@ export class OrderItemSummaryController {
         const pageHeader = mapPageHeader(request)
         try {
             logger.debug(`Retrieving summary for order/item [${orderId}/${itemId}] for user [${userId}]...`);
+            const basket: Basket = await getBasket(apiToken);
+            const basketLink: BasketLink = await getBasketLink(request, basket);
             const viewModel = await this.service.getOrderItem({
                 orderId,
                 itemId,
                 apiToken
             });
             logger.debug(`Retrieved summary for order/item [${orderId}/${itemId}] for user [${userId}]`);
-            return response.render(viewModel.template, { ...viewModel.data, ...pageHeader })
+            return response.render(viewModel.template, { ...viewModel.data, ...pageHeader, ...basketLink })
         } catch (error) {
             if (error instanceof Unauthorized) {
                 logger.info(`User [${userId}] is not authorised to retrieve summary for order/item [${orderId}/${itemId}]`);
