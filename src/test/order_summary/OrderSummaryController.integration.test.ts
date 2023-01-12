ce9209576d4ff2d8168d2f1a529ee7e7aa54e97f
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2023-01-12 14:17:07 +0000

    BI-12338: Fix test broken by merge from master.

--- src/test/order_summary/OrderSummaryController.integration.test.ts
+++ src/test/order_summary/OrderSummaryController.integration.test.ts
@@ -147,7 +147,7 @@ describe("OrderSummaryController", () => {
             // then
             const $ = cheerio.load(response.text);
             chai.expect(response.status).to.equal(404);
-            chai.expect($(".govuk-heading-xl").text()).to.contain("Page not found");
+            chai.expect($(".govuk-heading-xl").text()).to.contain("Sorry, there is a problem with the service");
             chai.expect(getBasketStub).to.have.been.called;
 
             verifyUserNavBarRenderedWithoutBasketLink(response.text);
