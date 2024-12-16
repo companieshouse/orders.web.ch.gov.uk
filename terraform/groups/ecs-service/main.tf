8e045b9d349d9db47b7cee9db3792cef9c143682
ch-ncolley <ncolley@companieshouse.gov.uk>
2024-12-16 11:16:57 +0000

    CC-1534 - Update healthcheck terraform for orders-web
    At the moment the healthcheck works but is not being found when checking ECS, I believe it's because in the Terraform configuration for this project the `use_task_container_healthcheck` is not set to true.

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -44,6 +44,9 @@ module "ecs-service" {
   lb_listener_arn           = data.aws_lb_listener.service_lb_listener.arn
   lb_listener_rule_priority = local.lb_listener_rule_priority
   lb_listener_paths         = local.lb_listener_paths
+
+  # Healthcheck configuration
+  use_task_container_healthcheck = true
   healthcheck_path          = local.healthcheck_path
   healthcheck_matcher       = local.healthcheck_matcher
 
