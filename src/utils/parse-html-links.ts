7f4f64e6e160050cf60739438d8e5c2a8b3b1420
aokunade <aokunade@companieshouse.gov.uk>
2026-01-25 19:52:14 +0000

    Return regexlink to previous value

--- src/utils/parse-html-links.ts
+++ src/utils/parse-html-links.ts
@@ -2,6 +2,6 @@
 export function parseHtmlLinks(input: string): string {
   if (!input) return "";
   // Regex to match [text](url)
-  const linkRegex = /\[([^\]]+)]\(([^)\s]+)\)/g;
+  const linkRegex = /\[([^\]]*)\]\(((?:[^()]*\([^)]*\))*[^)]*)\)/g;
   return input.replace(linkRegex, '<a class="govuk-link" target="_blank" href="$2">$1</a>');
 }
